1cea0ff4bb9c439079d792a20c0c93c3bc8b83b8
bristol <oro.eto.bristol@gmail.com>
2024-05-31 20:42:08 +0300

    Initial Commit

---
+++ FlashChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChatApp
+//
+//  Created by Apple M1 on 31.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
